6448a900c46887b451af5309a4238571f6e626c3
Michel De la Rosa <michel.delarosa@pluriza.com>
2019-02-27 17:30:13 -0500

    CSS changes on Footer and Header Component

--- web/lib/src/header_component/header.component.dart
+++ web/lib/src/header_component/header.component.dart
@@ -1,12 +1,15 @@
 import 'package:angular_bloc/angular_bloc.dart';
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
 
 @Component (
   selector: 'header-menu',
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.css'],
   directives: [
-    coreDirectives
+    coreDirectives,
+    MaterialIconComponent,
+    
   ],
   pipes: [BlocPipe]
 )
